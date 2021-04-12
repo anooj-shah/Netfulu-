3c3b214cd11457ed878b7504e97311865ac137a2
Anooj Shah <thisisanooj@gmail.com>
2021-04-12 17:25:03 -0500

    session page:

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import SessionPage from './components/SessionPage';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path='/home' component={HomePage} />
+        <Route exact path='/session/:sessionId' component={SessionPage} />
       </Switch>
     </Router>
   );
