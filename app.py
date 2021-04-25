f4c7317e55859020bc2620dd731f8dfa7862ee74
Ruben Uribe <uriben2000@gmail.com>
2021-04-25 17:03:44 -0500

    get the 100 most rated movies

--- app.py
+++ app.py
@@ -4,8 +4,15 @@ from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS 
 from firebase_admin import credentials, firestore, initialize_app
 import uuid
+import pandas as pd
+import os
+import requests
+from concurrent import futures
 
+# IMDB_key = os.environ["IMDB"]
 
+movies = pd.read_csv('./data/movies_metadata.csv',header=0,usecols=["id", "imdb_id", "original_title"])
+print(movies)
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app) #comment this on deployment
 api = Api(app)
@@ -15,10 +22,27 @@ default_app = initialize_app(cred)
 db = firestore.client()
 sessions_ref = db.collection('sessions')
 
-
 @app.route("/", defaults={'path':''})
 def serve(path):
   return send_from_directory(app.static_folder,'index.html')
+
+# imdb_url = "https://imdb-api.com/en/API/SearchMovie/"+IMDB_key +"/"
+# def get_imdb_img(url):
+#   res = requests.get(url)
+#   try:
+#       return res.json()
+#   except:
+#       return res.text
+
+@app.route("/getMovies", methods=['GET'])
+def getMovies():
+  samples = movies.sample(n=50)
+  res = {"movie_names": samples.original_title.to_numpy().tolist(), "id": samples.id.to_numpy().tolist()}
+  print(res)
+  return jsonify(res), 200
+  # images = []
+  # urls = [imdb_url + mov.original_title for i, mov in samples.iterrows()]
+  # return 
 
 @app.route('/test', methods=['GET'])
 def home():
@@ -34,6 +58,8 @@ def create():
     return jsonify({"success": True, "id": sessionId}), 200
   except Exception as e:
     return f"An Error Occured: {e}"
+
+
 
 
 # api.add_resource(HelloApiHandler, '/flask/hello')
