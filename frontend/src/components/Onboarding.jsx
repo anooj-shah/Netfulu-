be9393b90600c3dc7c08347afb8d2515d4f61c3a
Ruben Uribe <uriben2000@gmail.com>
2021-04-27 20:45:09 -0500

    nullify recs page

--- frontend/src/components/Onboarding.jsx
+++ frontend/src/components/Onboarding.jsx
@@ -28,7 +28,7 @@ function Onboarding(props){
       user: username
     }).then((res)=>{
       history.push({
-        pathname: "/recs/"+username+"/"+sessionId,
+        pathname: "/session/"+username+"/"+sessionId,
       });
     });
   }
--- frontend/src/components/RecsPage.jsx
+++ frontend/src/components/RecsPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 import {
   BrowserRouter as Router,
@@ -12,10 +12,21 @@ import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { firestore } from './firebaseContext';
 
 function RecsPage() {
-
+  const [users,setUsers] = useState();
+  const {sessionId, username} = useParams();
+  const url = "http://localhost:5000";
+  axios.post(url+"/joinSession", {
+    user: username,
+    session: sessionId
+  }).then((res)=>{
+    console.log("joined session as "+ username);
+    axios.post(url+"/getUsers",{
+      session: sessionId
+    })
+  });
   return (
     <div>
-
+      
     </div>
   )
 }
